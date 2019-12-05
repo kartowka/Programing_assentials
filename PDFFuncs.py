7b28b6ae8c2e39047c3e45462c20cb61b77079c3
Colvan <51700715+Colvan@users.noreply.github.com>
2019-12-05 19:00:07 +0200

    added crop file
    small changes

---
+++ PDFFuncs.py
@@ -0,0 +1,8 @@
+from PyPDF2 import PdfFileReader,PdfFileWriter
+
+reader=PdfFileReader('C:/Users/PROPHET/Downloads/Python_HW1 (1).pdf','r ')
+page=reader.getPage(0)
+print(page.cropBox.getLowerLeft())
+print(page.cropBox.getUpperLeft())
+print(page.cropBox.getUpperRight())
+print(page.cropBox.getLowerRight())

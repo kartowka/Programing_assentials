9fe880f838e6762d8c064ae03868f23e9883a042
Colvan <51700715+Colvan@users.noreply.github.com>
2019-12-06 13:50:51 +0200

    Update dataClasses.py
    added Builders
    and fixed inheritence

--- dataClasses.py
+++ dataClasses.py
@@ -1,25 +1,31 @@
-#Rakaz info
-class Coordinator(Lecturer):
-    department
-#Lecturer info
-class Lecturer(Student):
-    yearOfExprience
-    subjectOfTeaching
 #course info   
 class Cousre():
-    courseName
-    courseNumber
+    def _init_(courseName,courseNumber):
+        self.courseName=courseName
+        self.courseNumber=courseNumber
+ 
 #exam date and moed
 class Exam:
-    year
-    moed
+    def _init_(year):
+        self.year=year
 #question info     
 class Question(Exam):
-    NoQuestion
-    questionType
-    difficultyLevel
+    def _init_(NoQuestion,questionType,difficultyLevel):
+        self.NoQuestion=NoQuestion
+        self.questionType=questionType
+        self.difficultyLevel=difficultyLevel
 #student info
 class Student():
-    firstName
-    lastName
-    year
+    def _init_(firstName,lastName,year):
+        self.firstName=firstName
+        self.lastName=lastName
+        self.year=year
+#Lecturer info
+class Lecturer(Student):
+    def _init_(yearOfExprience,subjectOfTeaching):
+        self.yearOfExprience=yearOfExprience
+        self.subjectOfTeaching=subjectOfTeaching            
+#Rakaz info
+class Coordinator(Lecturer): 
+    def _init_(department):
+        self.department

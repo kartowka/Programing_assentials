1189f13e3882b9b32e06c940f5edfe0b24c9ae40
Colvan <51700715+Colvan@users.noreply.github.com>
2019-12-06 13:41:32 +0200

    Update PDFFuncs.py
    added Description

--- PDFFuncs.py
+++ PDFFuncs.py
@@ -1,9 +1,12 @@
 from PyPDF2 import PdfFileReader,PdfFileWriter
 from pdf2image import convert_from_path
-
+#FUNCTION TO CROP PDF TO DIFRENT PAGES
 def CropFile(FilePath):
+    #OPENING FILE
     inputPDF=PdfFileReader(FilePath,'rb')
+    #GETTING NUMBER OF PAGES
     pageNumber=inputPDF.getNumPages()
+    #LOOP TO RUN AND CROP NUM OF PAGES
     for i in range(pageNumber):
         output = PdfFileWriter()
         output.addPage(inputPDF.getPage(i))

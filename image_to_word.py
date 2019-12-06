506f57ec1c8d0d16708edeb9ab3d949bd948b07d
Colvan <51700715+Colvan@users.noreply.github.com>
2019-12-06 14:18:14 +0200

    Update image_to_word.py
    added univeral docname

--- image_to_word.py
+++ image_to_word.py
@@ -6,9 +6,10 @@ import docx
 def image_input(docName,imgPath,size=2):
     #docs file create
     document = docx.Document()
+    doc='.docx'
     #add a picture with 3 parameters receive(imgPath,width-const)
     document.add_picture(imgPath, width=Inches(2.0))
-    document.save(docName)
+    document.save("%s%s" %(docName,doc))
     return document
 
 #MUST INPUT DOC NAME WITH ENDING .DOCX

e158dc26a980aa7c0e2e02be3c36260ed8a38e14
kartowka <antonfley@gmail.com>
2019-12-08 10:49:45 +0200

    added filename,browse button,ok,close

--- pdffuncv2.py
+++ pdffuncv2.py
@@ -9,30 +9,39 @@ from pdf2image import convert_from_path
 class UploadBox:
     def __init__(self,root):
         self.fileName=StringVar()
-        self.label1=Label(root,text="please insert file with .pdf")
-        self.fileNameInput=Entry(root)
-        self.fileNameInput.insert(0,'please insert filename')
-        self.fileNameInput.bind("<Button-1>",lambda event: self.clear_entry(event, self.fileNameInput))
-        self.fileNameInput.place(x=120,y=30)
-        self.label1.place(x=100, y=0)
-        self.uploadTitle=Label(root,text="Upload PDF FILE")
-        self.uploadBtn=Button(root,text='OPEN',command=self.CropFile)
-        self.uploadBtn.place(x=160,y=60)
+        self.label1=Label(root,text="filename")
+        self.fileNameInput1=Entry(root,width=80)
+        self.fileNameInput1.insert(0,'enter filename')
+        self.fileNameInput1.bind("<Button-1>",lambda event: self.clear_entry(event, self.fileNameInput1))
+        self.fileNameInput1.place(x=120,y=30)
+        self.fileNameInput=Entry(root,width=80)
+        self.fileNameInput.insert(0,'')
+        #self.fileNameInput.bind("<Button-1>",lambda event: self.clear_entry(event, self.fileNameInput))
+        self.fileNameInput.place(x=120,y=50)
+        self.label1.place(x=120, y=0)
+        self.uploadBtn=Button(root,text='Browse',command=self.filePath)
+        self.uploadBtn.place(x=800,y=30)
         self.closeButton=Button(root,text="EXIT",command=self.close)
-        self.closeButton.place(x=220,y=60)
+        self.closeButton.place(x=160,y=80)
+        self.okButton=Button(root,text="Ok",command=self.CropFile)
+        self.okButton.place(x=120,y=80)
     def clear_entry(self,event, entry):
         entry.delete(0, END)
+    def filePath(self):
+        self.fileName=filedialog.askopenfilename()
+        self.fileNameInput.insert(0,self.fileName)
+        return self.fileName
     def CropFile(self):
-        self.filename = filedialog.askopenfilename()
+        #self.filename = filedialog.askopenfilename()
         #OPENING FILE
-        self.inputPDF=PdfFileReader(self.filename,'rb')
+        self.inputPDF=PdfFileReader(self.fileNameInput.get(),'rb')
         #GETTING NUMBER OF PAGES
         self.pageNumber=self.inputPDF.getNumPages()
         #LOOP TO RUN AND CROP NUM OF PAGES
         for i in range(self.pageNumber):
             output = PdfFileWriter()
             output.addPage(self.inputPDF.getPage(i))
-            with open(self.fileNameInput.get()+'%s.pdf' % i, "wb") as outputStream:
+            with open(self.fileNameInput1.get()+'%s.pdf' % i, "wb") as outputStream:
                  output.write(outputStream)
         messagebox.showinfo("Messege","file upload complete!")
     def close(self):
@@ -40,5 +49,5 @@ class UploadBox:
 window=Tk()
 mywin=UploadBox(window)
 window.title('SCE - PDF FILE2PAGE')
-window.geometry("400x200+10+10")
+window.geometry("1000x200+10+10")
 window.mainloop()

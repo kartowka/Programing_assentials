ae01e4f41f2273648b7195824b67e05b2eaa69d6
Colvan <51700715+Colvan@users.noreply.github.com>
2019-12-08 17:37:04 +0200

    Code Review
    added and delted funcs

--- test2.py
+++
@@ -1,5 +0,0 @@
-#pip3 install PySimpleGUI
-#pip install PySimpleGUI
-import PySimpleGUI as sg
-
-event, (filename,) = sg.Window('Get filename example'). Layout([[sg.Text('Filename')], [sg.Input(), sg.FileBrowse()], [sg.OK(), sg.Cancel()] ]).Read()

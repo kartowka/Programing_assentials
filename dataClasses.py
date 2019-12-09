e526a8b3ea827256b36461f7c1ee6b365ea29fe3
patrick14796 <57112628+patrick14796@users.noreply.github.com>
2019-12-09 14:53:36 +0200

    fixed inherithence and classes
    fixed classes

--- dataClasses.py
+++ dataClasses.py
@@ -3,31 +3,48 @@ class Course():
     def __init__(self,courseName,courseNumber):
         self.courseName=courseName
         self.courseNumber=courseNumber
+
 #exam date and moed
 class Exam(): 
-    def __init__(self,year):
+    def __init__(self,year,semester,moed):  #,year=None,semester=None,moed=None
         self.year=year
+        self.semester=semester
+        self.moed=moed
+
 #question info
 class Question(Exam):
-    def __init__(self,NoQuestion,questionType,difficultyLevel):
-        Exam.__init__(Exam,year=None)
-        self.NoQuestion=NoQuestion
-        self.questionType=questionType
-        self.difficultyLevel=difficultyLevel
+    def __init__(self,noQuestion=None,questionSubject=None,subQuestionSubject=None,difLvl=None,terms=None):
+        Exam.__init__(Exam,year=None,semester=None,moed=None)
+        self.noQuestion=noQuestion
+        self.questionSubject=questionSubject
+        self.subQuestionSubject=subQuestionSubject
+        self.difLvl=difLvl
+        self.terms=terms
+
+#user info
+class User():
+    def __init__(self, firstName, lastName, userName, password,phoneNumber):
+        User.firstName=firstName
+        User.lastName=lastName
+        User.userName=userName
+        User.password=password
+        User.phoneNumber=phoneNumber
+
 #student info
-class Student():
-    def __init__(self,firstName,lastName,year=0):
-        self.firstName=firstName
-        self.lastName=lastName
+class Student(User):
+    def __init__(self, year=0):
+        User.__init__(User, firstName=None, lastName=None, userName=None, password=None,phoneNumber=None)
         self.year=year
+
 #Lecturer info
-class Lecturer(Student,Course):
+class Lecturer(User,Course):
     def __init__(yearOfExprience):
-        Student.__init__(Student,firstName=None,lastName=None,year=None)
+        User.__init__(User,firstName=None,lastName=None , userName=None, password=None ,year=None)
         Course.__init__(Course,courseName=None,courseNumber=None)
         Lecturer.yearOfExprience=yearOfExprience          
+        
 #Rakaz info
-class Coordinator(Lecturer):
+class Coordinator(Lecturer,User):
     def __init__(self,department):
         Lecturer.__init__(self)
         self.department=department
